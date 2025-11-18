826ed12f1001ed7fcb2af20c42f455c0fae258b1
Dalton Kajander <daltonkajander@yahoo.com>
2025-11-17 21:24:42 -0500

    colors

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
-import powder as pd
+import powder as pd, colors
+import pygame
+
+FRAMERATE = 60
+CELL_SIZE = 5
+HEIGHT = 480
+WIDTH = 960
 
 if __name__ == "__main__":
     print("Welcome to the Powder Sim")
+    
+    #grid = pd.Grid(WIDTH, HEIGHT)
+    grid = pd.Grid(WIDTH // CELL_SIZE, HEIGHT // CELL_SIZE)
+    for i in range(0, grid.width, 2):
+        grid[i][0] = pd.Sand()
+        grid[i + 1][0] = pd.Water()
+    
+    # Screen initialization
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+    
+    while (running):
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT or (event.type == pygame.KEYUP and pygame.K_ESCAPE)): running = False
+            
+        screen.fill("black")
+        
+        # Game here
+        for y in range(grid.height):
+            for x in range(grid.width):
+                pygame.draw.rect(screen, grid[x][y].color.to_pygame_color(), (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+        
+        # Display and framerate
+        pygame.display.flip()
+        clock.tick(FRAMERATE)
+    
+    pygame.quit()
--- powder.py
+++ powder.py
@@ -1,9 +1,75 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from abc import ABC, abstractmethod
+import colors
 
 @dataclass
-class Element:
+class Element(ABC):
     name: str
+    color: colors.Color
+    density: float = 1.0
+    
+    def __str__(self): return f"{self.name}"
+    def __repr__(self): return self.__str__()
+    
+    @abstractmethod
+    def update(self) -> None: ...
     
     
 class Sand(Element):
-    ...
+    def __init__(self):
+        super().__init__("Sand", colors.color_from_hex("#c2b280"), density = 0.5)
+        
+    def update(self): ...
+    
+class Void(Element):
+    def __init__(self):
+        super().__init__("Void", colors.BLACK)
+        
+    def update(self): ...
+    
+class Water(Element):
+    def __init__(self):
+        super().__init__("Water", colors.color_from_hex("#4040ff"))
+        
+    def update(self): ...
+    
+
+@dataclass
+class Grid:
+    width: int = 10
+    height: int = 10
+    _board: list[list[Element]] = field(default_factory = list)
+    
+    def __post_init__(self):
+        self._board = [[Void() for x in range(self.height)] for y in range(self.width)]
+        
+    def __getitem__(self, row: int) -> list[Element]:
+        return self._board[row]
+    
+    def __setitem__(self, row: int, value: list[Element]) -> None:
+        self._board[row] = value 
+        
+    def __str__(self) -> str:
+        s = ""
+        for y in range(self.height):
+            for x in range(self.width):
+                s += self[x][y].__str__() + " "
+            s += "\n"
+        return s
+    
+    def copy(self) -> "Grid":
+        new_board = []
+        for x in range(self.width):
+            new_row = []
+            for y in range(self.height):
+                new_row.append(self[y][x])
+            new_board.append(new_row)
+            
+        new_grid = Grid(width = self.width, height = self.height)
+        new_grid._board = new_board
+        return new_grid
+    
+    def update(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                self[y][x].update()
